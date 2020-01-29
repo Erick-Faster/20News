c744676f5a44a480fe3b4991a259f7c31a7e64aa
Erick Faster <erick.fasterra@gmail.com>
2020-01-29 16:56:20 -0300

    Update 20NewsGroup_SVM.py

--- 20NewsGroup_SVM.py
+++ 20NewsGroup_SVM.py
@@ -13,23 +13,26 @@ import numpy as np
 twenty_train = fetch_20newsgroups(subset='train', shuffle=True)
 twenty_test = fetch_20newsgroups (subset='test', shuffle=True)
 
-'''Pre-Processing - Pipeline'''
-#Contagem
-#Frequencia
-#Classificador SVM
+'''V1 - Bag of words model não compacto'''
 
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.linear_model import SGDClassifier
-from sklearn.pipeline import Pipeline
+'''Contagem'''
+from sklearn.feature_extraction.text import CountVectorizer
+count_vect = CountVectorizer(stop_words='english', ngram_range=(1,2))
+X_train_counts = count_vect.fit_transform(twenty_train.data)
+X_train_counts.shape
 
-text_clf = Pipeline([('vect', CountVectorizer(ngram_range=(1,2),stop_words='english')),
-                     ('tfidf', TfidfTransformer(use_idf=True)),
-                     ('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=0.001, max_iter=5, random_state=42)),
-])
+'''Frequencia'''
+from sklearn.feature_extraction.text import TfidfTransformer
+tfidf_transformer = TfidfTransformer(use_idf=True)
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+X_train_tfidf.shape
+
+from sklearn import svm
+clf = svm.SVC(kernel= 'linear', gamma = 0.01, C = 0.001 )
 
 '''Treino'''
 
-text_clf = text_clf.fit(twenty_train.data, twenty_train.target)
+text_clf = clf.fit(X_train_tfidf, twenty_train.target)
 
 '''Teste'''
 
@@ -42,7 +45,3 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 
 precisao = accuracy_score(twenty_test.target, predicted_svm)
 matriz = confusion_matrix(twenty_test.target, predicted_svm)
-
-
-
-
